70fce8fc7c6d9cfef96475cecef78373c1954c66
1Jenx <mahmoudmrcr7@gmail.com>
2026-05-30 01:46:42 +0300

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,12 +1,12 @@
 -- Running the required queries --
 
---  Patients diagnosed with 'Fatty liver' in the last year --
+--  Patients diagnosed with 'Migraine' in the last year --
 SELECT Name
 FROM Patient
 WHERE PatientID IN (
     SELECT PatientID
     FROM Appointment
-    WHERE Diagnosis = 'Fatty liver' 
+    WHERE Diagnosis = 'Migraine' 
       AND Date >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
 );
 
@@ -14,18 +14,18 @@ WHERE PatientID IN (
 SELECT Clinic.Address
 FROM Clinic
 JOIN Department ON Clinic.DepartmentID = Department.DepartmentID
-WHERE Department.Name = 'Cardiology';
+WHERE Department.Name = 'Dermatology';
 
 -- Total money paid by patient with ID 12527 in the last 3 years --
 SELECT SUM(Cost) AS TotalPaid
 FROM Appointment
-WHERE PatientID = 12527 
+WHERE PatientID = 12529 
   AND Date >= DATE_SUB(CURDATE(), INTERVAL 3 YEAR);
 
 -- Sample UPDATE queries --
 UPDATE Patient
 SET Address = 'New Address', PhoneNumber = 'New PhoneNumber'
-WHERE PatientID = 12530;
+WHERE PatientID = 12533;
 
 UPDATE Appointment
 SET Status = 'Postponed'
